91682bdc793c59c44afebd4e7463ee232d6b048f
Drawhorn <Devin.Drawhorn@gartner.com>
2019-03-12 08:31:24 -0500

    Adding sum algo and compare method to coin_flip

--- coin_flip.py
+++ coin_flip.py
@@ -2,21 +2,43 @@
 
 """Generates a sequence of coin flips"""
 
+from timeit import default_timer as timer
 from random import choice
 from functools import reduce
 from operator import add
+from common import main_with_compare_and_arg as main
 
-def coin_flip_sequence(tries):
+def coin_flip_sequence_sum(tries):
     """Prints the results of coin flips and the totals of the results"""
-    assert tries >= 1
+    results = [choice([0, 1]) for _ in range(0, tries)]
+    tails = sum(results)
+    heads = len(results) - tails
+    result = ", ".join([["Heads", "Tails"][flip] for flip in results])
+    return result + "\n{:d} Heads; {:d} Tails".format(heads, tails)
+
+def coin_flip_sequence_reduce(tries):
+    """Prints the results of coin flips and the totals of the results"""
     results = [choice([0, 1]) for _ in range(0, tries)]
     tails = reduce(add, results, 0)
     heads = len(results) - tails
     result = ", ".join([["Heads", "Tails"][flip] for flip in results])
     return result + "\n{:d} Heads; {:d} Tails".format(heads, tails)
 
+def compare(funs, tries):
+    """Compares the execution times of functions in this module"""
+    times = {s: 0 for s, _ in funs.items()}
+    attempts = int(input("Attempts:\n"))
+    for _ in range(attempts):
+        for s, f in funs.items():
+            time = -timer()
+            f(tries)
+            time += timer()
+            times[s] += time / attempts
+    return "".join(["{:6s} average: {:.15f}\n".format(s, t) for s, t in times.items()])
+
 if __name__ == "__main__":
-    try:
-        print(coin_flip_sequence(int(input("How many coins do you want to flip?\n"))))
-    except AssertionError:
-        print("You gave an invalid argument")
+    main({
+        "sum": coin_flip_sequence_sum,
+        "reduce": coin_flip_sequence_reduce,
+        "compare": compare
+    }, "How many coins to flip?", int, lambda x: x >= 1)
