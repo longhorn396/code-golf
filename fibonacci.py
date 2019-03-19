dc7c3a1d9ca582941a6f515e1cd882624bb5b3e0
Drawhorn <Devin.Drawhorn@gartner.com>
2019-03-18 19:46:11 -0500

    Better dumb and new less_dumb fibonacci algos

--- fibonacci.py
+++ fibonacci.py
@@ -5,14 +5,31 @@
 from common import main_subf as main
 from common import compare_subfs as compare
 
-def dumb_fibonacci(n):
-    """Dumb solution; Returns only the Nth number"""
-    if n < 2:
-        return n
-    return dumb_fibonacci(n - 1) + dumb_fibonacci(n - 2)
+def dumb_fibonacci(n, seq=None):
+    """Dumb solution"""
+    if n == 1:
+        return 1, 0, [0]
+    if n == 2:
+        return 2, 1, [0, 1]
+    result1 = dumb_fibonacci(n - 1)
+    result2 = dumb_fibonacci(n - 2)
+    seq = result1[2]
+    seq.append(result1[1] + result2[1])
+    return n, seq[n - 1], seq[:n]
+
+def less_dumb_fibonacci(n, seq=None):
+    """Slight improvement on the dumb_fibonacci"""
+    if n == 1:
+        return 1, 0, [0]
+    if n == 2:
+        return 2, 1, [0, 1]
+    result = dumb_fibonacci(n - 1)
+    seq = result[2]
+    seq.append(seq[-1] + seq[-2])
+    return n, seq[n - 1], seq[:n]
 
 def iterative_fibonacci(n):
-    """Iterative solution; Returns N, the Nth number, and the sequence up to N"""
+    """Iterative solution"""
     seq = [0, 1]
     if n in seq:
         return n, seq[n - 1], seq[:n]
@@ -21,7 +38,7 @@ def iterative_fibonacci(n):
     return n, seq[-1], seq
 
 def recursive_fibonacci(n, seq=None):
-    """Recursive solution; Returns N, the Nth number, and the sequence up to N"""
+    """Recursive solution"""
     if seq is None:
         seq = [0, 1]
     if n > len(seq):
@@ -32,6 +49,7 @@ def recursive_fibonacci(n, seq=None):
 if __name__ == "__main__": # pragma: no cover
     main({
         "dumb": dumb_fibonacci,
+        "less_dumb": less_dumb_fibonacci,
         "iterative": iterative_fibonacci,
         "recursive": recursive_fibonacci,
         "compare": compare
