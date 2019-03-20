b1260716eec653c4d3c988e9974ef9f821ca74bb
Drawhorn <Devin.Drawhorn@gartner.com>
2019-03-19 19:51:40 -0500

    Fixing TODO in test_coin_flip

--- test_coin_flip.py
+++ test_coin_flip.py
@@ -8,23 +8,22 @@ from coin_flip import coin_flip_sequence_reduce, coin_flip_sequence_sum
 class coin_flipTestCase(unittest.TestCase):
     """Unit tests for coin_flip.py"""
 
-    def test_reduce(self):
-        """Test for the reduce algo"""
-        result = coin_flip_sequence_reduce(5)
+    def _helper(self, result):
+        """Logic for testing"""
         segments = result.split("\n")
-        # TODO: analyze segments[0]
+        seq = segments[0].split(", ")
+        self.assertEqual(len(seq), 5)
         num_heads = int(segments[1][0])
         num_tails = int(segments[1][9])
         assert num_heads + num_tails == 5
 
+    def test_reduce(self):
+        """Test for the reduce algo"""
+        self._helper(coin_flip_sequence_reduce(5))
+
     def test_sum(self):
         """Test for the sum algo"""
-        result = coin_flip_sequence_sum(5)
-        segments = result.split("\n")
-        # TODO: analyze segments[0]
-        num_heads = int(segments[1][0])
-        num_tails = int(segments[1][9])
-        assert num_heads + num_tails == 5
+        self._helper(coin_flip_sequence_sum(5))
 
 if __name__ == "__main__": # pragma: no cover
     unittest.main()
