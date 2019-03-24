77177e1cd8f0939e505496621dbb89bd1b0d405f
Devin Drawhorn <devin@drawhorn.com>
2019-03-24 11:26:01 -0500

    Implementing rovarspraket

---
+++ rovarspraket.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Sweedish term that translates to "Robber's Language";
+Consonats are doubled with an "o" between them"""
+
+from common import main
+
+def rovarspraket(english):
+    """Translates input to rovarspraket"""
+    for l, u in zip("bcdfghjklmnpqrstvwxyz", "BCDFGHJKLMNPQRSTVWXYZ"):
+        english = f"{u}o{l}".join(f"{l}o{l}".join(english.split(l)).split(u))
+    return english
+
+if __name__ == "__main__":
+    main(rovarspraket, str, "Input text to translate")
