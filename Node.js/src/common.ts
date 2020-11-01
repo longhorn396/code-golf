e28abac2201c87e825289a0ae24a903ae79f3e07
Devin Drawhorn <devin@drawhorn.com>
2020-11-01 10:50:36 -0600

    Node inOrder working with argv

--- Node.js/src/common.ts
+++ Node.js/src/common.ts
@@ -1,17 +1,31 @@
+import { exit } from 'process';
 import process = require('process');
-import inquirer = require('inquirer');
 
-export const main = (fun: Function, transform: Function, prompts: string[]) => {
+const stdin = process.openStdin();
+
+const getPromptResult = (prompt: string): string => {
+    let result: string = null;
+    console.log(prompt);
+    stdin.addListener("data", (d: any) => result = d.toString().trim());
+    return result;
+}
+
+export const toString = (input: any) => input.toString();
+
+export const main = (fun: Function, transform: Function, ...prompts: string[]) => {
     const argv = process.argv;
     let args: any[] = [];
     if (argv.length > 2) {
-        argv.slice(3, argv.length).forEach((arg: string) => args.push(transform(arg)));
+        argv.slice(2, argv.length).forEach((arg: string) => args.push(transform(arg)));
     } else {
-        inquirer.prompt(prompts).then((answer: any) => args.push(transform(answer)));
+        throw new Error("Prompting not supported at this time");
+        // prompts.forEach((prompt: string) => args.push(transform(getPromptResult(prompt))));
     }
     try {
         console.log(fun(...args));
+        exit(0);
     } catch(e) {
         console.error(e);
+        exit(1);
     }
 } 
---
+++ Node.js/src/inOrder.ts
@@ -0,0 +1,25 @@
+import { exit } from 'process';
+import { main, toString } from './common';
+
+export const inOrder = (text: string): string => {
+    const results: string[] = text.split(" ").map((word: string) => {
+        const order: boolean = wordInOrder(word);
+        const result: string = `${word} -${order ? '' : ' not'} in order`;
+        return result;
+    });
+    return results.join("\n");
+};
+
+// TODO: branch prediction vs simplicity?
+export const wordInOrder = (word: string): boolean => {
+    const characters = word.split("");
+    for (let i = 0; i < characters.length - 1; i++) {
+        if (characters[i] <= characters[i + 1]) {
+            continue;
+        }
+        return false;
+    }
+    return true;
+};
+
+main(inOrder, toString, "What word(s)?");
