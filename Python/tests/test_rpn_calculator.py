fe629bff3a67f389ab02b257748af8c16165a88a
Devin Drawhorn <devin@drawhorn.com>
2020-10-31 21:53:45 -0500

    Fixing test_rpn_calculator methods

--- Python/tests/test_rpn_calculator.py
+++ Python/tests/test_rpn_calculator.py
@@ -13,13 +13,13 @@ class RPNCalculatorTestCase(unittest.TestCase):
         self.assertEqual(pycg.iterative_rpn(stack), result)
         self.assertEqual(pycg.recursive_rpn_wrap(stack), result)
 
-    def simple_test_rpn_calculator(self):
+    def test_rpn_calculator_simple(self):
         """Simple test for the rpn_calculator algos"""
         self.help_test("2 3 +", 5)
 
-    def complex_test_rpn_calculator(self):
+    def test_rpn_calculator_complex(self):
         """Complex test for the rpn_calculator algos"""
-        self.help_test("2 3 + 3 * 1 - 7 \\ 2 %", 0)
+        self.help_test("2 3 + 3 * 1 - 7 / 2 %", 0)
 
 if __name__ == "__main__": # pragma: no cover
     unittest.main()
