6d2e3f839bf40618ccc12e3bab5054174523ab7e
Devin Drawhorn <devin@drawhorn.com>
2020-11-02 22:07:45 -0600

    Adding subfunction main to common

--- Node.js/src/common.ts
+++ Node.js/src/common.ts
@@ -32,4 +32,33 @@ export const main = (fun: Function, argType: ArgType, ...prompts: string[]) => {
         console.error(e);
         exit(1);
     }
-} 
+};
+
+export const mainSubf = (funs: any, argType: ArgType, check: Function, ...prompts: string[]) => {
+    const subfNames: string[] = Object.keys(funs);
+    let subfName: string = null;
+    let args: any[] = [];
+    const argTypeParsers = argTypeParseMap[argType];
+    if (argv.length > 2) {
+        subfName = argv[2];
+    } else {
+        console.log("What subfunction would you like to do?")
+        subfName = askPrompt(`Options: ${subfNames.join(", ")}`, question);
+    }
+    if (subfNames.includes(subfName)) {
+        if (argv.length > 3) {
+            argv.slice(3, argv.length).forEach((arg: string) => args.push(argTypeParsers.argv(arg)));
+        } else {
+            prompts.forEach((prompt: string) => args.push(askPrompt(prompt, argTypeParsers.prompt)));
+        }
+        if (args.every((value: any) => check(value))) {
+            // TODO: Compare
+            console.log(funs[subfName](...args));
+            exit(0);
+        } else {
+            throw new Error("Make sure you pass in valid arguments");
+        }
+    } else {
+        throw new Error("Make sure you pass in a valid subfunction");
+    }
+};
